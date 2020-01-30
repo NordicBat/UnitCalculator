4bcfc82a52d8d4c40acbbd6d78d3f4fcf817dc15
Gabriel Meira <gabriel.meira@2denker.de>
2020-01-30 11:16:58 +0100

    Initial Commit

---
+++ UnitCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitCalculator
+//
+//  Created by Gabriel Meira on 30.01.20.
+//  Copyright © 2020 Gabriel Meira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
